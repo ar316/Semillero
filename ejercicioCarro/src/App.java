38fbc84bae4ebc72be94ff4ad113d70b7bced5ee
Arnold Briñez <jgb199703@gmail.com>
2023-06-18 20:26:30 -0500

    incio x2
    inicio

---
+++ ejercicioCarro/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro c = new Carro("2010", "nizan", 200);  
+        System.out.println(c.Carroskm(c.getKm()));
+    }
+}
